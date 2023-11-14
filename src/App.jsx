faf62b6624104761181b3a89036209a527356514
Dylan Vazquez <vazquezadylan@gmail.com>
2023-11-13 23:51:32 -0800

    added ids for sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import './app.scss';
+import Navbar from './components/navbar/Navbar';
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section id="Home">
+        <Navbar />
+      </section>
+      <section id="Experience">Experience</section>
+      <section id="Services">Services</section>
+      <section id="Portfolio">Portfolio</section>
+      <section id="Contact">Contact</section>
+      <section id="About">About</section>
+    </div>
+  );
 };
 
 export default App;
